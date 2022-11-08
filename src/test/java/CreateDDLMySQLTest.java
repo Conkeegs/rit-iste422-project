32a0530f905b8cbb095a719fdb7451b2676e1288
JoshArqu <jba7083@g.rit.edu>
2022-11-07 21:45:22 -0500

    Added tests - don't commit to master
    I'm having an issue figuring out how user inputs are supposed to work here. Will talk it over and update tomorrow

--- src/test/java/CreateDDLMySQLTest.java
+++ src/test/java/CreateDDLMySQLTest.java
@@ -4,5 +4,53 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CreateDDLMySQLTest {
+  private StringBuffer nullSB;
     
+  @Before
+  public void setUp(){
+    nullSB = new StringBuffer();
+  }
+    
+  @Test
+  public void createNullEDDLTest(){
+		CreateDDLMySQL testObj = new CreateDDLMySQL();
+    testObj.createDDL();
+    assertEquals("Should be same as Null StringBuffer", testObj.sb.toString(), nullSB.toString());
+  }
+
+	@Test
+	public void convertStrBooleanToIntTest(){
+		CreateDDLMySQL testObj = new CreateDDLMySQL();
+    assertEquals("Should be 0", testObj.convertStrBooleanToInt("false"), 0);
+		assertEquals("Should be 1", testObj.convertStrBooleanToInt("true"), 1);
+	}
+
+	@Test
+	public void generateDatabaseNameTest(){
+		CreateDDLMySQL testObj = new CreateDDLMySQL();
+		assertEquals("Null Database should create default name", testObj.generateDatabaseName(), "MySQLDB");
+		assertEquals("EdgeConvertGUI readSuccess should be false", EdgeConvertGUI.getReadSuccess(), false);
+		assertEquals("generateDatabaseName should return empty string", testObj.generateDatabaseName(), "");
+		assertEquals("Name enered by user will be entered as database name", testObj.generateDatabaseName(), "name");
+	}
+
+	@Test
+	public void getDatabaseNameTest(){
+		CreateDDLMySQL testObj = new CreateDDLMySQL();
+		String input = testObj.generateDatabaseName();
+		assertEquals("Input should be database name", testObj.getDatabaseName(), input);
+	}
+
+	@Test
+	public void getProductNameTest(){
+		CreateDDLMySQL testObj = new CreateDDLMySQL();
+		assertEquals("Should return MySQL", testObj.getProductName(), "MySQL");
+	}
+
+	@Test
+	public void getSQLStringTest(){
+		CreateDDLMySQL testObj = new CreateDDLMySQL();
+		String str = testObj.getSQLString();
+		assertEquals("SQLString should be null", str, nullSB.toString());
+	}
 }
